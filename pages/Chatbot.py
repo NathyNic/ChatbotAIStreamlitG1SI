e8c308312c388671dc3fef473ae1402f41999eb7
NathyNic <nathalynicole07@hotmail.com>
2023-04-01 11:36:38 -0500

    Último cambio.

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -6,11 +6,13 @@ from streamlit_chat import message
 #API Key
 openai.api_key = st.secrets["api_secret"]
 
+context = "Starfly es un servicio de streamming para ver películas y series. Este servicio está disponible por el momento para dispositivos iOS y Android, así como para TV mediante ChromeCast y Apple TV. Existe dos tipos de suscripciones en este servicio: La suscripción mensual por 25 soles y la suscripción anual de 225 soles. En la suscripción anual ofrecemos un descuento de 3 meses gratis, incluido ya en el precio final. Si el cliente desea cancelar su suscripción, debe hacerlo llamando al número 505-1212 o mediante el correo starfly@suscription.com."
+
 #Creamos una función que generará las llamadas desde el API
 def generar_respuesta(prompt):
     comp = openai.Completion.create(
         engine = "text-davinci-003",
-        prompt = prompt,
+        prompt = context + "Q:" + prompt,
         max_tokens=1024,
         n = 1,
         temperature = 0.9,
@@ -30,7 +32,7 @@ if 'past' not in st.session_state:
 
 #Función para obtener el mensaje del usuario
 def obtener_texto():
-    input_text = st.text_input("Ud: ", "Hola, ¿cómo estás?", key="input")
+    input_text = st.text_input("Ud: ", "Hola, necesito ayuda", key="input")
     return input_text
 
 user_input = obtener_texto()
